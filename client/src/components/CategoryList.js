263a5b98a51ca22609a59d195600a4e5ded796b7
Cemal Can IŞÇI <cc.isci@hotmail.com>
2020-06-02 00:34:02 +0300

    ch

--- client/src/components/CategoryList.js
+++ client/src/components/CategoryList.js
@@ -7,9 +7,9 @@ import { faTimes } from '@fortawesome/free-solid-svg-icons'
     state={
         fields:[],
         isUpdate:false,
-        newFields:[]
+        newFields:[],
+        removedFields:[]
     }
-    newCategoryArr = [];
     componentDidMount() {
         this.setState({
             fields:[...this.props.initialCategories.categories]
@@ -24,8 +24,9 @@ import { faTimes } from '@fortawesome/free-solid-svg-icons'
     }
     changeFields = (i) => (e)=>{
         let newFields = [...this.state.fields];
-
+        console.log(newFields)
         newFields[i].title = e.target.value;
+        newFields[i].status = 'updated';
 
         this.setState({
              fields:[
@@ -34,30 +35,44 @@ import { faTimes } from '@fortawesome/free-solid-svg-icons'
      })
     }
      changeNewFields = (i) => (e)=>{
-        this.newCategoryArr[i].title = e.target.value;
-        let newFields = [...this.newCategoryArr];
-       this.setState({
-            newFields:[
-                ...newFields
-            ]
+        let value = e.target.value
+
+       this.setState(prevState=>{
+
+        let newArr = [...prevState.newFields];
+           newArr.forEach((q,index)=>{if(index===i){ q.title=value}})
+          return {
+              newFields:[...newArr]
+          }
+
         })
      }
      removeNewField =(i) =>(e)=>{
-        let fields = [];
-        this.newCategoryArr.splice(i,1);
-         fields = [...this.newCategoryArr];
         this.setState({
-            newFields:[...fields]
+            newFields:[...this.state.newFields.filter((q,index)=>{return index!==i})]
+        })
+    }
+    removeField =(i) =>(e)=>{
+        this.setState(prevState=>{
+            return {
+            removedFields:[
+                ...prevState.removedFields,
+                ...this.state.fields.filter((q,index)=>{return index===i})
+            ],
+            fields:[...this.state.fields.filter((q,index)=>{return index!==i})]}
         })
     }
      addNewCategory = ()=>{
         let newCategory = {
              title:'Kategori ismi giriniz'
          }
-         this.newCategoryArr.push(newCategory);
-
-         this.setState({
-            newFields:[...this.newCategoryArr]
+         this.setState(prevState=>{
+            return {
+                newFields:[
+                    ...prevState.newFields,
+                    newCategory
+                ]
+            }
         })
      }
     save = ()=>{
@@ -69,22 +84,25 @@ import { faTimes } from '@fortawesome/free-solid-svg-icons'
                <div className="w-100 bg-dark d-flex flex-row align-items-center justify-content-around p-2">
                    <h5 className="text-white">İşlemler : </h5>
                 <button onClick={this.activateUpdate} className={(!this.state.isUpdate ? 'btn-success ' : 'btn-danger ') + 'btn btn-sm'}>{!this.state.isUpdate ? 'Düzenlemeyi Aktifleştir' : 'Düzenlemeyi Kapat'}</button>
-                   {this.state.isUpdate ? <button className="btn btn-sm btn-warning text-white" onClick={this.save}>Kaydet</button> : ''}
-                   <button onClick={this.addNewCategory} className="btn btn-sm btn-danger">Yeni Ekle</button>
+                   <button onClick={this.addNewCategory} className="btn btn-sm btn-info">Yeni Ekle</button>
+                   <button onClick={this.save} className="btn btn-sm btn-warning text-white">Kaydet</button>
                </div>
                 <div className="d-flex flex-row  wrap">
                 {        this.state.fields.map((category,i) => {
                 return <div className={(i%2===0 ?'bg-first ' : 'bg-second ' ) + 
-                        'col-md-4 w-100 d-flex justify-content-center align-items-center' }
-                        key={category._id}>{this.state.isUpdate ? <input name={category.title} onChange={this.changeFields(i)} defaultValue={category.title} /> : <span className=" text-white">{category.title}</span>}
+                        'col-md-4 w-100 d-flex justify-content-around align-items-center' }
+                        key={category._id}>{this.state.isUpdate ? <input name={category.title} onChange={this.changeFields(i)} defaultValue={category.title} /> : <span className=" text-white">{category.title}</span>
+                }
+                        <button onClick={this.removeField(i)} className="btn btn-danger btn-sm ml-2"><FontAwesomeIcon icon={faTimes} /></button>
+                        
                  </div>
         })}
                    {
                     this.state.newFields && this.state.newFields.length ? 
                     this.state.newFields.map((category,i)=>{
                             return <div className={((i+this.state.fields.length)%2===0 ?'bg-first ' : 'bg-second ' ) + 
-                        'col-md-4 w-100 d-flex justify-content-center align-items-center' }
-                        key={i}><input name={category.title} onChange={this.changeNewFields(i)} defaultValue={category.title} />
+                        'col-md-4 w-100 d-flex justify-content-around align-items-center' }
+                        key={i}><input name={category.title} onChange={this.changeNewFields(i)} value={category.title} />
                         <button onClick={this.removeNewField(i)} className="btn btn-danger btn-sm ml-2"><FontAwesomeIcon icon={faTimes} /></button>
                  </div> 
                      }) : ''        
