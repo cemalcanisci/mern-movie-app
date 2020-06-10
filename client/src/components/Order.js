436ac916d7f7ed318b7215a94b10cc86d73142a0
Cemal Can IŞÇI <cc.isci@hotmail.com>
2020-06-10 23:38:10 +0300

    ch

--- client/src/components/Order.js
+++ client/src/components/Order.js
@@ -1,17 +1,39 @@
 import React, { Component } from 'react'
-import { DndProvider } from 'react-dnd'
-import { HTML5Backend } from 'react-dnd-html5-backend'
+import {SortableContainer, SortableElement} from 'react-sortable-hoc';
+const SortableItem = SortableElement(({value}) => <li>{value}</li>);
+ 
+const SortableList = SortableContainer(({items}) => {
+  return (
+    <ul>
+      {items.map((value, index) => (
+        <SortableItem key={`item-${value}`} index={index} value={value} />
+      ))}
+    </ul>
+  );
+});
+const arrayMove = (oldIndex,newIndex)=>{
+    console.log(oldIndex);
+    console.log(newIndex);
+}
 export default class Order extends Component {
+    state = {
+        items: ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5', 'Item 6'],
+      };
+      onSortEnd = ({oldIndex, newIndex}) => {
+          let newArr = [...this.state.items];
+         let spliced = newArr.splice(oldIndex,1)[0];
+        //  newArr.splice(oldIndex,1);
+        newArr.splice(newIndex,0,spliced);
+            console.log(newArr);
+        this.setState({
+            items:newArr
+        })
+        console.log(oldIndex);
+        console.log(newIndex)
+      };
     render() {
-        return (
-            <div>
-                <DndProvider backend={HTML5Backend}>
-					<span>Cemo</span>
-					<span>Cemo</span>
-					<span>Cemo</span>
-					<span>Cemo</span>
-				</DndProvider>
-            </div>
-        )
+        return <SortableList items={this.state.items} onSortEnd={this.onSortEnd} />;
+
+
     }
 }
